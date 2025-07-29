4b1f440efbb2083e858356710b8d117c1a74aab6
skarrea <bendik.s.abrahamsen@ntnu.no>
2025-07-29 12:21:16 +0200

    added tab autocomplete

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,5 +1,6 @@
 local lspconfig = require("lspconfig")
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 -- LSP: pyright
 lspconfig.pyright.setup {}
@@ -16,6 +17,8 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  	['<Tab>'] = cmp.mapping.select_next_item(),
+  	['<S-Tab>'] = cmp.mapping.select_prev_item(),
   }),
   sources = cmp.config.sources({
     { name = "nvim_lsp" },
